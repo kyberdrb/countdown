1bcc9b87c180e6bb3f16015b42c213ad862b4652
kyberdrb <kyberdrb@gmail.com>
2020-03-21 16:35:13 +0100

    Fix alarm sound

--- countdown.sh
+++ countdown.sh
@@ -1,13 +1,15 @@
 #/bin/sh
 
 MINUTES=$1
-SONG=$2
+SONG_ARG=$2
+SONG=${SONG_ARG:="/home/laptop/git/kyberdrb/countdown/Bongos_Sample_Sound_Effect_Loops_HD-mgJkQ5NUQ7Y.webm"}
 
 MINUTES_IN_SEC=$(( ${MINUTES} * 60 ))
 echo "Countdown is set to ${MINUTES_IN_SEC} seconds"
 echo "Start time: $(date)"
 
-sleep ${MINUTES_IN_SEC}
+#sleep ${MINUTES_IN_SEC}
+
 echo "End time:   $(date)"
 echo
 
