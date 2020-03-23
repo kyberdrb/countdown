9bfe838a6ea5f60b98beab2c94a2ce0e1387d9f6
kyberdrb <kyberdrb@gmail.com>
2020-03-23 13:10:13 +0100

    Fix delay

--- countdown.sh
+++ countdown.sh
@@ -8,7 +8,7 @@ MINUTES_IN_SEC=$(( ${MINUTES} * 60 ))
 echo "Countdown is set to ${MINUTES_IN_SEC} seconds"
 echo "Start time: $(date)"
 
-#sleep ${MINUTES_IN_SEC}
+sleep ${MINUTES_IN_SEC}
 
 echo "End time:   $(date)"
 echo
